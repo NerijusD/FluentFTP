9679c7d6309b3cdc52e301dad5a35a18693f4b46
Nerijus Dzindzeleta <nd@mcb.dk>
2017-10-24 18:12:35 +0200

    compression helper works with .NET 2.0

--- FluentFTP/Helpers/FtpGzip.cs
+++ FluentFTP/Helpers/FtpGzip.cs
@@ -7,13 +7,19 @@ namespace FluentFTP.Helpers
     {
         public static byte[] Decompress(byte[] data)
         {
-            using (var input = new MemoryStream(data))
+	        byte[] buffer = new byte[4 * 1024]; // 4k is traditional choice
+
+			using (var input = new MemoryStream(data))
             {
                 using (var output = new MemoryStream())
                 {
-                    using (var deflateStream = new DeflateStream(input, CompressionMode.Decompress))
-                    {
-                        deflateStream.CopyTo(output);
+					using (var deflateStream = new DeflateStream(input, CompressionMode.Decompress))
+					{
+						int read;
+						while ((read = deflateStream.Read(buffer, 0, buffer.Length)) > 0)
+						{
+							output.Write(buffer, 0, read);
+						}
                     }
                     return output.ToArray();
                 }
@@ -22,13 +28,19 @@ namespace FluentFTP.Helpers
 
         public static byte[] Compress(byte[] data)
         {
-            using (var input = new MemoryStream(data))
+	        byte[] buffer = new byte[4 * 1024]; // 4k is traditional choice
+
+			using (var input = new MemoryStream(data))
             {
                 using (var output = new MemoryStream())
                 {
                     using (var deflateStream = new DeflateStream(input, CompressionMode.Compress))
                     {
-                        deflateStream.CopyTo(output);
+	                    int read;
+	                    while ((read = deflateStream.Read(buffer, 0, buffer.Length)) > 0)
+	                    {
+		                    output.Write(buffer, 0, read);
+	                    }
                     }
                     return output.ToArray();
                 }
