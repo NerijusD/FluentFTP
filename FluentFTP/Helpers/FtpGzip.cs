8a4b900e55b24f8679cb8e20e876c1ac13b7cf5e
Nerijus Dzindzeleta <nd@mcb.dk>
2017-10-24 17:10:59 +0200

    added gzip helper

---
+++ FluentFTP/Helpers/FtpGzip.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace FluentFTP.Helpers
+{
+    public class FtpGzip
+    {
+        public static byte[] Decompress(byte[] data)
+        {
+            using (var input = new MemoryStream(data))
+            {
+                using (var output = new MemoryStream())
+                {
+                    using (var deflateStream = new DeflateStream(input, CompressionMode.Decompress))
+                    {
+                        deflateStream.CopyTo(output);
+                    }
+                    return output.ToArray();
+                }
+            }
+        }
+
+        public static byte[] Compress(byte[] data)
+        {
+            using (var input = new MemoryStream(data))
+            {
+                using (var output = new MemoryStream())
+                {
+                    using (var deflateStream = new DeflateStream(input, CompressionMode.Compress))
+                    {
+                        deflateStream.CopyTo(output);
+                    }
+                    return output.ToArray();
+                }
+            }
+        }
+    }
+}
